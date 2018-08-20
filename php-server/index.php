b2168b71b025870c6ccfd79dd9924d1aa0640b0f
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-08-20 14:35:57 +0200

    Change of structure

---
+++ php-server/index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE HTML>
+<!--
+	Miniport by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Enrico Zammit Lonardelli</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="/css/main.css" />
+                <link rel="stylesheet" href="/css/font-awesome.min.css">
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116690956-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-116690956-1');
+		</script>
+
+	</head>
+	<body>
+
+			<!-- Nav -->
+			<nav id="nav">
+				<ul class="container">
+					<li><a href="/">Homepage</a></li>
+					<li><a href="#work">About me</a></li>
+					<li><a href="#portfolio">Projects</a></li>
+					<li><a href="#contact">Contact</a></li>
+					<li><a href="https://www.linkedin.com/in/enricozammitl" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+					<li><a href="https://github.com/enricozammitlon" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+					<li><a href="/login.php">Personal Area</a></li>
+
+				</ul>
+			</nav>
+
+		<!-- Home -->
+			<div class="wrapper style1 first">
+				<article class="container" id="top">
+					<div class="row">
+						<div class="4u 12u(mobile)">
+							<span class="image fit"><img src="/img/enrico.jpg" alt="" /></span>
+						</div>
+						<div class="8u 12u(mobile)">
+							<header>
+								<h1>Hi. I'm <strong>Enrico</strong>.</h1>
+							</header>
+							<p>I'm a 20 year old <strong>Maltese-Italian</strong> student in my third year of studies in <strong>Theoretical Physics</strong> at the <strong>University of Manchester</strong>.</p>
+							<a href="#work" class="button big scrolly">Learn about what I do</a>
+						</div>
+					</div>
+				</article>
+			</div>
+
+		<!-- Work -->
+			<div class="wrapper style2">
+				<article id="work">
+					<header>
+						<h2>Here's all the stuff I do.</h2>
+						<p>From academia to free-time projects to hobbies...</p>
+					</header>
+					<div class="container">
+						<div class="row">
+							<div class="4u 12u(mobile)">
+								<section class="box style1">
+									<span class="icon featured fa-desktop"></span>
+									<h3>Programming</h3>
+									<p>My programming skills include  C++, Java, Python, HTML5, CSS and a range of frameworks such as Django and Flask. I am also an advanced user of linux systems including but not limited to Debian and CentOs distributions.</p>
+								</section>
+							</div>
+							<div class="4u 12u(mobile)">
+								<section class="box style1">
+									<span class="icon featured fa-graduation-cap"></span>
+									<h3>University</h3>
+									<p>I am in my second year of Theoretical Physics at the University of Manchester. I have studied modules such as Advanced Linear Dynamics, Quantum Mechanics and Solid State Physics. I am currently projected for a First Degree.</p>
+								</section>
+							</div>
+							<div class="4u 12u(mobile)">
+								<section class="box style1">
+									<span class="icon featured fa-soccer-ball-o"></span>
+									<h3>Hobbies</h3>
+									<p>I practice a range of sports at amateur level.I play in my university's 11-a-side campus footall league and I am the captain of a 5-a-side league football team. I also enjoy boxing,running and cycling.</p>
+								</section>
+							</div>
+						</div>
+					</div>
+					<footer>
+						<a href="#portfolio" class="button big scrolly">See some of my recent work</a>
+					</footer>
+				</article>
+			</div>
+
+		<!-- Portfolio -->
+			<div class="wrapper style3">
+				<article id="portfolio">
+					<header>
+						<h2>Here’s some stuff I made recently.</h2>
+						<p>This section includes anything I have done from programming to scientific research.</p>
+					</header>
+					<div class="container">
+						<div class="row">
+							<div class="4u 12u(mobile)">
+								<article class="box style2">
+									<a href="http://tubeamp.me" class="image featured"><img src="/img/pic02.jpg" alt="" /></a>
+									<h3><a href="http://tubeamp.me">Tubeamp</a></h3>
+									<p>
+									TubeAmp is a WebApp that creates Spotify playlists based on journeys on the London Underground.
+									It can be invoked remotely by sending an SMS message to 07418 340 203
+									It can also be accessed at TubeAmp.me
+									<p>
+								</article>
+							</div>
+							<div class="4u 12u(mobile)">
+								<article class="box style2">
+									<a href="https://github.com/enricozammitlon/KingMe" class="image featured"><img src="/img/pic03.jpg" alt="" /></a>
+									<h3><a href="https://github.com/enricozammitlon/KingMe">KingMe</a></h3>
+									<p>A checkers game to play against an artificial intelligence that uses a Min-Max based approach. Although a very naive AI system, it was a very useful introduction to artificial intelligence systems. </p>
+								</article>
+							</div>
+							<div class="4u 12u(mobile)">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="/img/pic02.jpg" alt="" /></a>
+									<h4><a href="#">Different deformation mechanisms leading to auxetic behavior exhibited by missing rib square grid structures</a></h4>
+									<p>This is the first (hopefully of many) academic paper I am coauthor on. This is a result of an internship at the Univerity of Malta in summer of 2017 where I worked under the supervision of Dr.P.S.Farrugia.</p>
+								</article>
+							</div>
+						</div>
+						<div class="row">
+							<div class="4u 12u(mobile)">
+								<article class="box style2">
+									<a href="https://github.com/enricozammitlon/BookWorm" class="image featured"><img src="/img/pic04.jpg" alt="" /></a>
+									<h3><a href="https://github.com/enricozammitlon/BookWorm">BookWorm</a></h3>
+									<p>An app designed for android to maintain a database of books for a personalised book library. Has the ability to scan details directly from barcode. </p>
+								</article>
+							</div>
+							<div class="4u 12u(mobile)">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="/img/pic01.jpg" alt="" /></a>
+									<h3><a href="#">My Personal Server</a></h3>
+									<p>The server on which this website is hosted and the design of it all. This is a small blog about the details as to how I did it and the updates that follow.</p>
+								</article>
+							</div>	
+							<div class="4u 12u(mobile)">
+								<article class="box style2">
+									<a href="https://github.com/enricozammitlon/MusicalCities" class="image featured"><img src="/img/pic02.jpg" alt="" /></a>
+									<h3><a href="https://github.com/enricozammitlon/MusicalCities">Musical cities of Great Britain.</a></h3>
+									<p>A Simple Python program that takes a list of the top 100 Great Britain cities and turns into a piano where the keys are the cities sorted by population. Also plots the map using Basemap library.</p>
+								</article>
+							</div>
+			</div>
+			<footer>
+				<a href="#contact" class="button big scrolly">Get in touch with me</a>
+			</footer>
+		</article>
+		<!-- Contact -->
+			<div class="wrapper style4">
+				<article id="contact" class="container 75%">
+					<header>
+						<h2>Contact me.</h2></p>
+					</header>
+					<div>
+						<!--
+						{% if success %}
+							<p style="background-color: #00cc99">Thank you for your message. I'll get back to you shortly!</p>
+						{% else %}
+						{% for message in get_flashed_messages() %}
+						<div class="flash" style="background-color: #FBB0B0">{{ message }} </div>
+						{% endfor %}
+						{% endif %}
+						<div class="row">
+							<div class="12u">
+								<form method=POST action="\index#contact">
+									{{ form.hidden_tag() }}
+									<div>
+										<div class="row">
+											<div class="6u 12u(mobile)">
+												<label style="color:#fff" for="name">Name</label>
+												{{ form.name }}
+											</div>
+											<div class="6u 12u(mobile)">
+												<label style="color:#fff" for="email">Email</label>
+												{{ form.email }}
+											</div>
+										</div>
+										<div class="row">
+											<div class="6u 12u(mobile)">
+												<label style="color:#fff" for="subject">Subject</label>
+												{{ form.subject }}
+											</div>
+										</div>
+										<div class="row">
+											<div class="12u">
+												<label style="color:#fff" for="message">Message</label>
+												{{ form.message }}
+											</div>
+										</div>
+										<div class="row 200%">
+											<div class="12u">
+												<ul class="actions">
+													<li>{{ form.submit }}</li>
+													<li><input type="reset" value="Clear Form" class="alt" /></li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+						-->
+
+						<div class="row">
+							<div class="12u">
+								<hr />
+								<h3>Find me on ...</h3>
+								<ul class="social">
+									<li><a href="https://www.linkedin.com/in/enrico-zammit-lonardelli/" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+									<li><a href="https://github.com/enricozammitlon" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+								</ul>
+								<li>Website created and maintained by <a href="http://www.enricozl.me">Enrico Zammit Lonardelli</a></li>
+							</div>
+						</div>
+					</div>
+				</article>
+			</div>
+
+		<!-- Scripts -->
+		<script src="/js/jquery.min.js"></script>
+		<script src="/js/jquery.scrollex.min.js"></script>
+		<script src="/js/jquery.scrolly.min.js"></script>
+		<script src="/js/skel.min.js"></script>
+		<script src="/js/util.js"></script>
+		<script src="/js/main.js"></script>
+	</body>
+</html>
