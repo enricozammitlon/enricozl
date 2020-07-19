2239c9cceab586b3eeb3f43ca28e3265c0cb2a19
Enrico Zammit <enrico.zammitl@gmail.com>
2020-07-19 21:38:55 +0200

    Fix linting issues

--- src/containers/contact.jsx
+++ src/containers/contact.jsx
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
+import React from 'react';
 
-export default class Contact extends Component {
-
-	render() {
-
-		return(
-			<div>
-			<p>You can email me at : enrico.zammitl@gmail.com or enricozl@protonmail.com or find me on LinkedIn.</p>
-			</div>
-		)
-	}
+export default function Contact() {
+  return (
+    <div>
+      <p>
+        You can email me at : enrico.zammitl@gmail.com or enricozl@protonmail.com or find me on
+        LinkedIn.
+      </p>
+    </div>
+  );
 }
